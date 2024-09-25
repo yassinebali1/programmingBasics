08e980ea2f2a2adadc726fd9f5e4437e112aa255
yassine bali <ybali3385@gmail.com>
2024-09-25 12:42:18 +0200

    commite

---
+++ Programming_Basics/Week_1/Day_2/Core/Code Challenge Predict Arrays (Core)/Snippet 1.js
@@ -0,0 +1,21 @@
+// Code Snippet 1
+var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+console.log(contactInfo);
+["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345]// result function
+
+// Code Snippet 2
+var produce = ["apples", "oranges"];
+var frozen = ["broccoli", "ice cream"];
+frozen.push("hashbrowns");
+console.log(frozen);
+["broccoli", "ice cream","hashbrowns"] // result function 
+
+// Code Snippet 3
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zorro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+["Bambi", "Beetlejuice", "Toy Story","Zorro"] // result function
+
+
+
